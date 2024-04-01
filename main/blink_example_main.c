ef94a099d0cd83d2044f7b42cacb4d90f21e5067
Justin Reina <jreina@whisker.com>
2024-04-01 14:52:21 -0400

    (C)    clean & trim source formatting

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -1,4 +1,4 @@
-/* Blink Example
+/* Blink Example - FreeRTOS example
 
    This example code is in the Public Domain (or CC0 licensed, at your option.)
 
@@ -6,6 +6,9 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
+
+//--------------------------------- Includes ---------------------------------//
+
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -14,78 +17,75 @@
 #include "led_strip.h"
 #include "sdkconfig.h"
 
+
+//-------------------------------- Constants ---------------------------------//
+
 static const char *TAG = "example";
 
-/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO CONFIG_BLINK_GPIO
 
-static uint8_t s_led_state = 0;
+//------------------------------- Definitions --------------------------------//
 
-#ifdef CONFIG_BLINK_LED_RMT
+#define BLINK_GPIO 	(12)						/* @prev CONFIG_BLINK_GPIO    */
 
-static led_strip_handle_t led_strip;
 
-static void blink_led(void)
-{
-    /* If the addressable LED is enabled */
-    if (s_led_state) {
-        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
-        /* Refresh the strip to send data */
-        led_strip_refresh(led_strip);
-    } else {
-        /* Set all LED off to clear all pixels */
-        led_strip_clear(led_strip);
-    }
-}
+//-------------------------------- Variables ---------------------------------//
 
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
-    /* LED strip initialization with the GPIO and pixels number*/
-    led_strip_config_t strip_config = {
-        .strip_gpio_num = BLINK_GPIO,
-        .max_leds = 1, // at least one LED on board
-    };
-    led_strip_rmt_config_t rmt_config = {
-        .resolution_hz = 10 * 1000 * 1000, // 10MHz
-    };
-    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
-    /* Set all LED off to clear all pixels */
-    led_strip_clear(led_strip);
-}
+uint8_t s_led_state = 0;
 
-#elif CONFIG_BLINK_LED_GPIO
 
-static void blink_led(void)
+//--------------------------------- Routines ---------------------------------//
+/*
+ * @todo 	header
+ */
+void blink_led(void)
 {
     /* Set the GPIO level according to the state (LOW or HIGH)*/
     gpio_set_level(BLINK_GPIO, s_led_state);
+
+    return;
 }
 
+
+/*
+ * @todo 	header
+ */
 static void configure_led(void)
 {
     ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
+
     gpio_reset_pin(BLINK_GPIO);
+
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    return;
 }
 
-#endif
 
+/*
+ * @todo 	header
+ */
 void app_main(void)
 {
 
     /* Configure the peripheral according to the LED type */
     configure_led();
 
+	printf("\r\n\r\n\r\n\r\n");
+	printf("//----------------------------------------------------------------------------//\r\n");
+	printf("//-                                                                          -//\r\n");
+	printf("//----------------------------------------------------------------------------//\r\n");
+	printf("Hello world!\n\r\n\r");
+
     while (1) {
         ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
+
         blink_led();
+
         /* Toggle the LED state */
         s_led_state = !s_led_state;
+
         vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
     }
 }
+
